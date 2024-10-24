aac7b148acaa9ffbbd9d4b459ab5dc2b03e8295d
Virender Chauhan <bikuchauhan786+github@gmail.com>
2024-10-24 20:31:16 +0530

    updated cors policy

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://delievery-app-topaz.vercel.app",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
     allowedHeaders: ["Authorization", "Content-Type"],
